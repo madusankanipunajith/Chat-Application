2e1237d234917490b5d2197780705e48562d0d75
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2021-10-02 00:14:42 +0530

    Initialized the server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+// middleware
+app.use(express.static(publicDirectoryPath));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
