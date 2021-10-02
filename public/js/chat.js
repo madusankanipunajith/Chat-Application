b064f1e301adec1e28ed3db39e8925f9232f231c
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2021-10-02 11:34:16 +0530

    Challenge completed - 1 => greeting message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,26 @@
-io() 
+const socket = io();
+
+// socket.on('countUpdated', (count)=>{
+//     console.log('The count has been updated!', count);
+// })
+
+// document.querySelector('#increment').addEventListener('click', ()=>{
+//     console.log('clicked');
+//     socket.emit('increment');
+// })
+
+socket.on('welcomeMessage', (message)=>{
+    console.log(message);
+})
+
+socket.on('replyMessage', (reply)=>{
+    console.log(reply);
+})
+
+document.querySelector("#welcome-form").addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = document.querySelector('input').value;
+    // if you want to access different input tags it is better to use below method
+    // const message = e.target.elements.message;
+    socket.emit('welcome', message);
+})
--- src/index.js
+++ src/index.js
@@ -13,9 +13,25 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 // middleware
 app.use(express.static(publicDirectoryPath));
 
+const message = 'Hello Welcome to Jithson chat application';
+
 // configuring socket io connection
-io.on('connection', ()=>{
+io.on('connection', (socket)=>{
     console.log('New websocket connection is running');
+    // socket.emit('countUpdated', count);
+
+    // socket.on('increment', ()=>{
+    //     count++;
+    //     // socket.emit('countUpdate, count)
+    //     io.emit('countUpdated', count);
+    // })
+
+    socket.emit('welcomeMessage', message);
+
+    socket.on('welcome', (reply)=>{
+        io.emit('welcomeMessage', reply);
+    })
+
 })
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
