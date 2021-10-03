27840452201f855aa8d4cff0ed48f328ac4cd90a
Madusanka Nipunajith <56679833+madusankanipunajith@users.noreply.github.com>
2021-10-03 09:22:57 +0530

    Templates Genarated

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ io.on('connection', (socket)=>{
 
     socket.on('sendLocation', (location, callback)=>{
         //io.emit('welcomeMessage', `latitude : ${location.latitude} and longitude: ${location.longitude}`);
-        io.emit('welcomeMessage', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+        io.emit('locationMessage', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
         callback();
     })
 
